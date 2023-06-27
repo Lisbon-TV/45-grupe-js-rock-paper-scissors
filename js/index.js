147e4d8938c2c3f6d1d1af1d5969971ed992f772
Lisbon <lisbon@dublin.com>
2023-06-27 11:56:26 +0300

    Clean up

--- js/index.js
+++ js/index.js
@@ -36,10 +36,38 @@ function checkWinner() {
     if(player == computer) {
         return "Try again! 👀";
     } else if(computer == "👊") {
-        return(player == "🤚") ? "You Win! 🤩" : "You Lose 🥱";
+        return(player == "🤚") ? "You Win! 🤩" : "Sorry, you Lose! 🥱";
     } else if(computer == "🤚") {
-        return(player == "✌") ? "You Win! 😁": "You Lose 🤔";
+        return(player == "✌") ? "You Win! 😁": "You Lose! 🤔";
     } else if(computer == "✌") {
-        return(player == "👊") ? "You Win! 😉" : "You Lose 😪";
+        return(player == "👊") ? "You Win! 😉" : "Ajajai.. you Lose! 😪";
     }
 }
+
+// const bodyDOM = document.getElementById('fullReset');
+
+
+// resetDOM.addEventListener('click', () => {
+//     bodyDOM.insertHTML = `<body>
+//     <div id="fullReset">
+//     <div id="game">
+//         <h1  class="text" id="playerText">Player:</h1>
+//         <h1  class="text" id="compText">Computer:</h1>
+//         <h1  class="text" id="rezText">Result:</h1>
+//     </div>
+
+//     <div class="buttons">
+//     <button class="btn">👊</button>
+//     <button class="btn">🤚</button>
+//     <button class="btn">✌</button>
+//     </div>
+    
+
+//     <div id="reset">
+//     <button calss="button ">Play from scratch!</button>
+//     </div>
+
+//     </div>
+
+//     <script src="./js/index.js" type="module" defer></script>
+// </body>`});
